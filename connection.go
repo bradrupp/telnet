22a7b11945d00c662e61e2d1b34abaa3bf7c9aba
MH_ProDev <124782291+tester2024@users.noreply.github.com>
2023-12-16 17:22:20 +0330

    update and bug fixes

--- connection.go
+++ connection.go
@@ -195,7 +195,7 @@ func (c *Connection) read(b []byte) (n int, err error) {
 				endIAC(i)
 			}
 			continue
-		} else if c.iac && c.cmd == SB && ch == SE && c.buf[i-1] == IAC {
+		} else if c.iac && i-1 >= 0 && c.cmd == SB && ch == SE && c.buf[i-1] == IAC {
 			// Handle SB command with SE option
 			if h, ok := c.OptionHandlers[c.option]; ok {
 				h.HandleSB(c, c.buf[c.r:i-1])
